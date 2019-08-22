ff50bf8a4ea8478046a4ce616bfe7f3a34d2564e
zhenyuan <936451076@qq.com>
2019-08-22 14:18:28 +0800

    修改请求头解决多线程请求过于频繁导致的ConnectionError

--- tools.py
+++ tools.py
@@ -31,7 +31,8 @@ dataModel = {
     'other': ['详情','更多']
 }
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+    'Connection': 'close',
 }
 
 
